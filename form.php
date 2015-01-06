4d63a82e24b2bbfcf205fe6e439615d093257a61
Sulman Baig <sulmanpersonal@gmail.com>
2015-01-06 23:10:21 +0100

    Integrating with PayPal

---
+++ form.php
@@ -0,0 +1,21 @@
+<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+    <input type="hidden" name="cmd" value="_s-xclick">
+    <input type="hidden" name="hosted_button_id" value="49XGWGKZ5CQK2">
+    <table>
+        <tr>
+            <th>
+                <input type="hidden" name="on0" value="Size">
+                <label for="os0">Size</label>
+            </th>
+
+            <td>
+                <select name="os0" id="os0">
+                    <option value="Small">Small </option>
+                    <option value="Medium">Medium </option>
+                    <option value="Large">Large </option>
+                </select>
+            </td>
+        </tr>
+    </table>
+    <input type="submit" value="Add to Cart" name="submit">
+</form>
